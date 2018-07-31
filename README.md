fabfd25612be901baf81eb9daea997146df8ebf3
Keila <keiladaianefernandes24@gmail.com>
2018-07-31 01:30:19 -0300

    Tipos básicos de variáveis / arrays

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+# Compilar arquivo typescript manualmente pelo terminal
 
+* Abre o terminal
+* Executa o comando **tsc nomedoarquivoparacompilar**
+* Ele irá gerar um arquivo **nomedoarquivoparacompilar.js** com tudo compilado 
+
+# Compilar arquivo em typescript automaticamente no Visual Studio Code
+
+* Abra o terminal do VSC e digite **tsc --init** na pasta do seu projeto para criar o arquivo **tsconfig.json** (se você não tiver um).
+* Pressione **Ctrl+Shift+B** para abrir uma lista de tarefas no VS Code
+* selecione tsc: watch - tsconfig.json ou tsc: monitorar - tsconfig.json.
+* Feito! Seu projeto é recompilado em cada arquivo salvo.
+ 
+Você pode ter vários arquivos tsconfig.json em sua área de trabalho e executar várias compilações de uma só vez, se desejar (por exemplo, frontend e backend separadamente).
---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+//Tipos
+var teste = true;
+var id = 2;
+//......................
+var decimal = 5.3;
+var hexadecimal = 0xf00d;
+var binário = 10;
+var octal = 484;
+//.......................
+var nome = "Keila";
+var carro = 'camaro';
+var text = "<p>Name: " + nome + "</p>\n                    <p>Car: " + carro + "</p>";
+document.body.innerHTML = text;
+//.......................
+//Arrays
+var fruits = ["Apple", "Watermelon", "Lemon"];
+console.log(fruits);
+var frutas = ["Apple", "Watermelon", "Lemon"];
+console.log(frutas);
+//.........................
+var color;
+(function (color) {
+    color[color["Red"] = 1] = "Red";
+    color[color["Green"] = 2] = "Green";
+    color[color["Blue"] = 3] = "Blue";
+})(color || (color = {})); //Exemplo onde é usado: estado cívil, sexo
+console.log(color[2]);
+console.log(color.Red);
+//........................
+var list = 23;
+console.log(list);
+list = "String...";
+console.log(list);
+//.......................
+//void -> não retorna nada
+function printNumber(num) {
+    alert(num);
+}
+printNumber(32);
